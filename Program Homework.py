2cd2c1d696d186f4f05050969bd3a3a961a61917
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-03-02 12:40:47 -0600

    Update Program Homework.py

--- Program Homework.py
+++ Program Homework.py
@@ -16,36 +16,22 @@ seasonSummer = 6, 7, 8
 seasonFall = 9, 10, 11
 seasonWinter = 12, 1, 2
 
-if mensaje1 == 3:
-    print(mensaje1)
-elif mensaje1 == 4:
-    print(mensaje1)
-elif mensaje1 == 5:
-    print(mensaje1)
-elif mensaje2 == 6:
-    print(mensaje2)
-elif mensaje2 == 7:
-    print(mensaje2)
-elif mensaje2 == 8:
-    print(mensaje2)
-elif mensaje3 == 9:
-    print(mensaje3)
-elif mensaje3 == 10:
-    print(mensaje3)
-elif mensaje3 == 11:
-    print(mensaje3)
-elif mensaje4 == 12:
-    print(mensaje4)
-elif mensaje4 == 1:
-    print(mensaje4)
-elif mensaje4 == 2:
-    print(mensaje4)
-pass
+def get_season_message(month): 
+    if month in seasonSpring:
+        return mensaje1
+    elif month in seasonSummer:
+        return mensaje2
+    elif month in seasonFall:
+        return mensaje3
+    elif month in seasonWinter:
+        return mensaje4
+    else:
+        return "Invalid month"
 
 mensajeUnico = ("The month is")
 
 if __name__ == '__main__':
-    print(f"{mensajeUnico} {My_months[month-1]} and {mensaje1, mensaje2, mensaje3, mensaje4}")
+    print(f"{mensajeUnico} {My_months[month-1]} and {get_season_message(month)}")
     
 
     
