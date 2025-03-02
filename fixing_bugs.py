f2139fcdbc200e24e4fd8cd4a42e72ee89c00810
Arquimedes <105887093+Arquime16@users.noreply.github.com>
2025-03-02 13:30:13 -0600

    Update fixing_bugs.py
    update

--- fixing_bugs.py
+++ fixing_bugs.py
@@ -8,15 +8,15 @@
 #"You entered a" if the user enters a
 #"You entered a b" if the user enters b
 #"Well it wasn't an a or b" if the user enters anything else
-#This is my file Done!
+ #This is my file Done!
 
 
-selection = input("Enter a lowercase letter")
+selection = input("Enter a lowercase letter: ")
 
-if selection != "a": 
+if selection == "a": 
    print("You entered a")
 elif selection == "b": 
-   print("You entered a b")
+   print("You entered b")
 else:
    print("Well it wasn't an a or b.")
  
